3574ea27bc801f815d833f9d757d389363af0841
Aleksi Huusko <aleksihuusko@mbp.local>
2021-03-09 17:12:38 +0200

    front build

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.post('/api/reminders/', (req, res) => {
   res.json(reminder);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
