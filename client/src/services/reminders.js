b22c98fd388aecfe22f706739f646e8f563ad45d
Aleksi Huusko <aleksihuusko@mbp.local>
2021-03-09 18:05:41 +0200

    fullstack fix

--- client/src/services/reminders.js
+++ client/src/services/reminders.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/reminders';
+const baseUrl = '/api/reminders';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
